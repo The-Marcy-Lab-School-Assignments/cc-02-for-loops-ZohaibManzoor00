686346aac57d9acd8f6e7c6a8298464a94961d79
Zohaib <zohaibmanzoor1700@gmail.com>
2021-09-29 14:51:31 +0000

    Completed

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 //1
-function countToTen(){
-  
-}
+let countToTen = () => { for(let i = 1; i <=10; i++) console.log(i) }
+//countToTen()
 
 //2
-function countFromOne(){
-  
-}
+let countFromOne = (num) => { for(let i = 1; i <= num; i++) console.log(i) }
+//countFromOne(3)
 
 //3
-function countEveryEven(){
-  
+let countEveryEven = (num) => { for(let i = 1; i <= num; i++) {
+    if( i % 2 === 0){
+        console.log(i)
+        }
+    } 
 }
+//countEveryEven(10)
 
 //4
-function countEveryOdd(){
-  
+let countEveryOdd = (num) => { for(let i = 1; i <= num; i++) {
+    if( i % 2 !== 0){
+        console.log(i)
+        }
+    } 
 }
+//countEveryOdd(10)
+
 
 //5 
-function countEvens(){
-  
+let countEvens = (array) => {
+    let evenInt = [];
+    for(let i = 0; i < array.length; i++){
+        if(i % 2 !== 0){
+            evenInt.push(array[i])
+        } return evenInt.length 
+    } 
+    
 }
+
+
+console.log(countEvens([1,4,5,6,7,2]))
 
